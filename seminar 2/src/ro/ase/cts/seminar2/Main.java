aaa2a3cf47c0d337ac0a50770532710d7769c0b7
FloreaLaura <florealaura18@stud.ase.ro>
2021-03-04 08:22:54 +0200

    before break

---
+++ seminar 2/src/ro/ase/cts/seminar2/Main.java
@@ -0,0 +1,15 @@
+package ro.ase.cts.seminar2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//CurrentAccount c=new CurrentAccount(300,"IBAN1");
+	//	System.out.println("suma disponibila este: "+c.getBalance());
+		System.out.println("Creditul maxim pentru cont curent este " + CurrentAccount.MAX_CREDIT);
+
+		
+		//nu pot instantia o clasa abstracta
+		//Account a=new Account();
+	}
+
+}
