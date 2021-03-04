699dc4184b4482db56b768ed220e6cb2da20abb9
FloreaLaura <florealaura18@stud.ase.ro>
2021-03-04 09:21:53 +0200

    sem2

--- seminar 2/src/ro/ase/cts/seminar2/CurrentAccount.java
+++ seminar 2/src/ro/ase/cts/seminar2/CurrentAccount.java
@@ -1,5 +1,8 @@
 package ro.ase.cts.seminar2;
 
+import ro.ase.cts.seminar2.exceptii.IllegalTransferException;
+import ro.ase.cts.seminar2.exceptii.InsufficientFundsException;
+
 public class CurrentAccount extends BankAccount {
 	public static double MAX_CREDIT=5000;
 	
@@ -16,20 +19,29 @@ public class CurrentAccount extends BankAccount {
 	@Override
 	public void deposit(double amount) {
 //		this.setBalance(getBalance()+ amount);
-		this.balance +=amount; 
+		this.balance += amount; 
 		
 	}
 
 	@Override
-	public void withdraw(double amount) {
-		// TODO Auto-generated method stub
-		
+	public void withdraw(double amount) throws InsufficientFundsException {
+		if(this.balance >= amount) {
+			this.balance -= amount;
+		}
+		else {
+			throw new InsufficientFundsException("fonduri insuficiente");
+		}
 	}
 
 	@Override
-	public void transfer(double amount, Account destination) {
-		// TODO Auto-generated method stub
-		
+	public void transfer(double amount, Account destination) throws IllegalTransferException, InsufficientFundsException {
+		if(((BankAccount)destination).iban.equals(this.iban)){
+			throw new IllegalTransferException("conturile coincid");
+		}
+		else {
+			this.withdraw(amount);
+			destination.deposit(amount);
+		}
 	}
 
 	
