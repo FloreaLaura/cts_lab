d8b39a7f7f12a1cca014b798294da6abe3e695d5
FloreaLaura <florealaura18@stud.ase.ro>
2021-03-18 09:25:47 +0200

    seminar 4

---
+++ seminar4/src/ro/ase/cts/seminar4/ShoppingCart.java
@@ -0,0 +1,15 @@
+package ro.ase.cts.seminar4;
+
+public class ShoppingCart {
+	private static ShoppingCart instance;
+	
+	private ShoppingCart() {}
+
+	public static ShoppingCart getInstance() {
+		if(instance==null) {
+			instance=new ShoppingCart();
+		}
+		return instance;
+	}
+	
+}
