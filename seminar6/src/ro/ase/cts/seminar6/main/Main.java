c433a17dc2c94243a2a8a2fdd87bad36c1336645
FloreaLaura <florealaura18@stud.ase.ro>
2021-04-08 09:26:05 +0300

    final seminar7

--- seminar6/src/ro/ase/cts/seminar6/main/Main.java
+++ seminar6/src/ro/ase/cts/seminar6/main/Main.java
@@ -11,60 +11,60 @@ import ro.ase.cts.seminar6.builder.TechProductFactory;
 public class Main {
 
 	public static void main(String[] args) {
-		Cart myShoppingCart = Cart.getInstance("shopping");
+			Cart myShoppingCart = Cart.getInstance("shopping");
 
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Selectati categoria de produse:\n Produse tech \n Produse office");
-		String userPreferences = scan.nextLine();
-		Product myProduct = null;
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Selectati categoria de produse:\n Produse tech \n Produse office");
+			String userPreferences = scan.nextLine();
+			Product myProduct = null;
 
-		AbstractProductFactory productFactory = null;
-		if (userPreferences != null) {
-			if (userPreferences.equalsIgnoreCase("tech")) {
-				productFactory = new TechProductFactory();
-			}
-		}
-
-		System.out.println(productFactory.getCatalog());
-		userPreferences = scan.nextLine();
-
-		for (int i = 0; i < 2; i++) {
-			userPreferences = scan.nextLine();
-			try {
-				int selectedId = Integer.valueOf(userPreferences);
-				if (myShoppingCart.products.isEmpty()) {
-					myProduct = productFactory.makeProduct(selectedId);
+			AbstractProductFactory productFactory = null;
+			if (userPreferences != null) {
+				if (userPreferences.equalsIgnoreCase("tech")) {
+					productFactory = new TechProductFactory();
 				}
+			}
 
-				for (Product p : myShoppingCart.products) {
-					if (p instanceof TechProduct) {
-						TechProduct tempProduct = (TechProduct) p;
+			System.out.println(productFactory.getCatalog());
+			//userPreferences = scan.nextLine();
 
-						if (tempProduct.getId() == selectedId) {
+			
+				for(int i=0;i<2;i++) {
+					userPreferences = scan.nextLine();
+				try {
+					int selectedId = Integer.valueOf(userPreferences);
+					if(myShoppingCart.products.isEmpty()) {
+						myProduct=productFactory.makeProduct(selectedId);
+					}
+					for(Product p: myShoppingCart.products) {
+						if(p instanceof TechProduct)
+						{
+							TechProduct tempProduct=(TechProduct)p;
+						
+						if(tempProduct.getId()==selectedId) {
 							try {
-								myProduct = (Product) tempProduct.clone();
+								myProduct=(Product) tempProduct.clone();
 							} catch (CloneNotSupportedException e) {
+								// TODO Auto-generated catch block
 								e.printStackTrace();
 							}
-						} else {
-							myProduct = productFactory.makeProduct(selectedId);
+						}else {
+							myProduct=productFactory.makeProduct(selectedId);
+						}
 						}
 					}
+					myProduct = productFactory.makeProduct(selectedId);
+				} catch (NumberFormatException e) {
+					System.err.println("Selectie invalida");
 				}
-				myProduct = productFactory.makeProduct(selectedId);
-			} catch (NumberFormatException e) {
-				System.err.println("Selectie invalida");
-
 			}
-
+			
 			if (myProduct != null) {
 				myShoppingCart.products.add(myProduct);
 			}
-		}
 
-		for (Product p : myShoppingCart.products) {
-			System.out.println(p.getDescription());
-		}
+			for (Product p : myShoppingCart.products) {
+				System.out.println(p.getDescription());
+			}	
 	}
-
 }
